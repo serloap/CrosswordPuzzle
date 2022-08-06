cb73d30c6a918b35008f0bb37d9d1a1bddb0cf82
Sergio Loaiza <sergio0195@hotmail.com>
2022-08-06 01:14:05 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+// import { useState } from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./styles.css";
 
+const Test = () => <div>TEEEEEST</div>;
+
 export default function App() {
+    // const [columnsQty, setColumnsQty] = useState(0);
+    // const [rowsQty, setRowsQty] = useState(0);
+
+    // const [words, setWords] = useState;
+    
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {/* <div>
+        <form>
+          <input type="text" name="rows" className="form-control" />
+          <input type="text" name="columns" className="form-control" />
+        </form>
+      </div> */}
+      <Test />
+      {/* <h1>Helgfhgjflo CodeSandbox saving test krtdstrsrejhjk</h1> */}
+      <h2>goooooooood</h2>
+      {/* <h2>Start editing to see some magic happen!</h2> */}
     </div>
   );
 }
